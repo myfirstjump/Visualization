b5facc091f4b263925581935ca86a748e88b0f48
Edward Chen <myfirstjump@gmail.com>
2017-07-27 12:29:27 -0500

    Add files via upload

--- 20170718ETL/etl_manpower_ggplot2.R
+++ 20170718ETL/etl_manpower_ggplot2.R
@@ -1,18 +1,19 @@
 rm(list=ls())
-#install.packages("dplyr")
+#install.packages("ggplot2")
 
 library(ggplot2)
 library(dplyr)
+library('reshape2')
 target = read.csv("R/etl/toolRank.csv")
 top = target[1:10,]
 
 
 df = data.frame()
 for (tool in top$toolList) {
-  df = rbind(df, top[top$toolList==tool,], top[top$toolList==tool,], top[top$toolList==tool,], top[top$toolList==tool,])
+  xf = rbind(df, top[top$toolList==tool,], top[top$toolList==tool,], top[top$toolList==tool,], top[top$toolList==tool,])
 }
 man_power = c("518", "104", "yes123", "1111")
-df = cbind(df, man_power)
+xf = cbind(xf, man_power)
 
 mp = c()
 for(tool in top$toolList) {
@@ -20,9 +21,21 @@ for(tool in top$toolList) {
 }
 
 total_by_mp = unlist(mp, use.names = FALSE)
-df = cbind(df, total_by_mp)
-ggplot(df, aes(x=toolList, y=total_by_mp, fill=man_power)) + geom_bar(stat='identity') + scale_x_discrete(limits=top$toolList[1:10])
+xf = cbind(xf, total_by_mp)
+ggplot(xf, aes(x=toolList, y=total_by_mp, fill=man_power)) + geom_bar(stat='identity') + scale_x_discrete(limits=top$toolList[1:10])
 top$toolList
 
 
 #ggplot(top, aes(x=toolList, y=total)) + geom_bar(stat='identity') + scale_x_discrete(limits=top$toolList)
+
+mf = as.data.frame(t(top))
+mf = mf[-1,]
+names(mf) = unlist(mf[1,])
+mf = mf[c(-1,-6),]  #去掉工具 & total
+mf$mp = c("518", "104", "yes123", "1111")
+
+mf.m = melt(mf,id.vars = 'mp')
+g = ggplot(mf.m, aes(mp, value))   
+g + geom_bar(aes(fill = variable), position = "stack", stat="identity")
+
+
